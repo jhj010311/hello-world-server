bea83918e01b108bd96106c85214a45348d5f50f
DESKTOP-GR883KV\user <jhj010311@gmail.com>
2025-07-16 14:55:49 +0900

    first commit

---
+++ src/main/java/com/example/helloworldserver/HelloWorldServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.helloworldserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldServerApplication.class, args);
+    }
+
+}
