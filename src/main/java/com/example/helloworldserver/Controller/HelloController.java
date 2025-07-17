e19656acaefbe1395740a75de11ec6431dcb0a12
DESKTOP-GR883KV\user <jhj010311@gmail.com>
2025-07-17 14:37:26 +0900

    CORS 허용 설정

--- src/main/java/com/example/helloworldserver/Controller/HelloController.java
+++ src/main/java/com/example/helloworldserver/Controller/HelloController.java
@@ -5,10 +5,12 @@ import com.example.helloworldserver.repository.HelloRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@CrossOrigin(origins = "http://localhost:3000")
 @RequiredArgsConstructor
 public class HelloController {
 
